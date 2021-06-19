84fde22bfece229442bdcb3b899f63b8e41b2c5d
Ruchir <ruchir@pdx.edu>
2021-06-18 23:57:29 -0700

    better version for 11.cpp (two pointer)

--- 11.cpp
+++ 11.cpp
@@ -21,3 +21,29 @@ public:
         return maximum;
     }
 };
+
+
+//Improved version:
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int height_length = height.size();
+        if(height_length <= 1)
+            return 0;
+        int left_pointer = 0, right_pointer = height_length-1, minimum = 0;
+        int max_area = 0, temp_area = 0, diff = 0;
+        while(left_pointer < right_pointer) {
+            minimum = min(height[left_pointer],height[right_pointer]);
+            diff = right_pointer - left_pointer;            
+            temp_area = minimum * diff;
+            if(temp_area > max_area)
+                max_area = temp_area;
+            if(height[left_pointer] < height[right_pointer])
+                ++left_pointer;
+            else
+                --right_pointer;   
+        }
+        return max_area;
+    }
+};
