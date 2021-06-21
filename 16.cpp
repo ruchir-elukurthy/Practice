a7f39fbd2a532388ae4494b1fa7633e238093a3f
Ruchir <ruchir@pdx.edu>
2021-06-20 22:22:24 -0700

    faster version of 3 sum closest done

--- 16.cpp
+++ 16.cpp
@@ -24,3 +24,37 @@ public:
         return closest_sum;  
     }
 };
+
+
+//2 pointer version, more optimal soln
+
+class Solution {
+public:
+    //nums = [-4,-1,1,2] after sorting.
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int length = nums.size();
+        int sum = 0, closest_sum = 0;
+        int diff = 0, smallest_diff = INT_MAX;
+        int left_pointer = 1, right_pointer = length-1;
+        for(int i = 0; i < length; ++i) {
+            left_pointer = i+1;
+            right_pointer = length - 1;
+            while(left_pointer < right_pointer) {
+                sum = nums[i] + nums[left_pointer]+nums[right_pointer];
+                if(sum == target)
+                    return sum;
+                diff = target - sum;                                    //4
+                if(abs(diff) < abs(smallest_diff)) {                    
+                    closest_sum = sum;                                  //-3
+                    smallest_diff = diff;
+                }
+                if(sum > target)
+                    --right_pointer;
+                else
+                    ++left_pointer;
+            }
+        }
+        return closest_sum;
+    }
+};
