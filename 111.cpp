ca339af0f4380cf31ae752c8f50ba1c8654f4da7
Ruchir <ruchir@pdx.edu>
2021-08-13 23:28:00 -0700

    111 done using bfs

--- 111.cpp
+++ 111.cpp
@@ -13,3 +13,33 @@ public:
             return 1 + min(left_depth,right_depth);
     }
 };
+
+
+
+//BFS solution.
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        queue<TreeNode *> nums;
+        if(!root)
+            return 0;
+        int min_depth = 0;
+        nums.push(root);
+        while(!nums.empty()) {
+            ++min_depth;
+            int size = nums.size();
+            while(size > 0) {
+                TreeNode * temp = nums.front();
+                --size;
+                nums.pop();
+                if(!temp->left && !temp->right)
+                    return min_depth;
+                if(temp->left)
+                    nums.push(temp->left);
+                if(temp->right)
+                    nums.push(temp->right);
+            }
+        }
+        return min_depth;
+    }
+};
