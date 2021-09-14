b2f2fcbfad5caed12c7cade6840133823030ce59
Ruchir <ruchir@pdx.edu>
2021-09-14 14:54:41 -0700

    optimal version done

--- 152.cpp
+++ 152.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int left = 1, right = 1;
-        int maximum = INT_MIN, len = nums.size();
-        for(int i = 0; i < len; ++i) {
-            left *= nums[i];
-            right *= nums[len-i-1];
-            maximum = max(maximum,max(left,right));
-            if(left == 0) left = 1;
-            if(right == 0) right = 1;
+        int temp_max = nums[0], temp_min = nums[0], curr_max = nums[0], hold = 0;
+        int len = nums.size();
+        for(int i = 1; i < len; ++i) {
+            hold = temp_max; 
+            temp_max = max(nums[i], nums[i]*temp_max);
+            temp_max = max(temp_max, nums[i]* temp_min);
+            temp_min = min(nums[i], nums[i]*temp_min);
+            temp_min = min(temp_min, nums[i] * hold);
+            cout<<temp_max<<" "<<temp_min<<endl;
+            curr_max = max(curr_max, temp_max);
         }
-        return maximum;
+        return curr_max;
+        
     }
 };
