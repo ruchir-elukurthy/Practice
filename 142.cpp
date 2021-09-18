99c668e8cc3139887c3ab8e47f147786da60cd78
Ruchir <ruchir@pdx.edu>
2021-09-17 19:47:46 -0700

    used two pointer approach

--- 142.cpp
+++ 142.cpp
@@ -1,19 +1,20 @@
-// 3 -> 0, 2-> 1, 0 -> 3, -4 -> 4,
-// 2
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_map<ListNode*,int> my_map;
-        ListNode * current = head;
-        int i = 0;
-        while(current != NULL) {
-            if (my_map.find (current) == my_map.end())
-                my_map.insert({current, i});
-            else 
-                return current;
-            ++i;
-            current = current->next;
+        ListNode * slow = head;
+        ListNode * fast = head;
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) {
+                slow = head;
+                while(fast != slow) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
         }
-        return NULL;
+        return nullptr;
     }
 };
